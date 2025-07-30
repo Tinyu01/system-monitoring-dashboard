bcdea3b21940084a99fd6468ced17d23c191c617
MASINGITA OTTIS MALULEKE <216135982@edu.vut.ac.za>
2025-07-30 13:59:46 +0200

    Update Dashboard.js

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -6,11 +6,7 @@ import ResourceView from './ResourceView';
 import ARPreview from './ARPreview';
 
 function Dashboard() {
-  const { data, connect } = useWebSocket('https://vercel.com/masingita-ottis-malulekes-projects/system-monitoring-dashboard-osme/6fuBr6UNd99WZU2TQHXGWDMZR8Py');
-
-  useEffect(() => {
-    connect();
-  }, [connect]);
+  const { data } = useWebSocket('https://vercel.com/masingita-ottis-malulekes-projects/system-monitoring-dashboard-osme/6fuBr6UNd99WZU2TQHXGWDMZR8Py');
 
   return (
     <div className="container mx-auto p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
